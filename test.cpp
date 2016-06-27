168e08dbd45bcfb9542068175f82508a4ca87b28
Thomas Schlögl <schloegl.thomas@aon.at>
2016-06-27 20:03:27 +0200

    .) in test.cpp: changed correctness tests to performance tests

--- test.cpp
+++ test.cpp
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
 	{
 		for(int z = 0; z < NUMBER_OF_INDIV_TESTRUNS; z++)
 		{
-			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentCorrSort.txt correctness"));
-			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentBackSort.txt correctness"));
+			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentCorrSort.txt performance"));
+			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentBackSort.txt performance"));
 
-			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_80_10_10.txt correctness"));
-			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_70_20_10.txt correctness"));
-			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_60_20_20.txt correctness"));
-			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_50_35_15.txt correctness"));
-			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_40_30_30.txt correctness"));
+			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_80_10_10.txt performance"));
+			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_70_20_10.txt performance"));
+			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_60_20_20.txt performance"));
+			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_50_35_15.txt performance"));
+			system(strcat(strcat(strcpy(buffer, "./listTest "), std::to_string(threads).c_str()), " ListContentRandom_40_30_30.txt performance"));
 		}
 	}
 
